5c6565c70c871aad8f21ea4f62b76ff42e9bedb8
YoussefWilliam <youssef.william997@gmail.com>
2022-08-26 15:26:48 +0200

    adding user validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
-class Article < ActiveRecord::Base
+class Article < ApplicationRecord
     # Add validation where title is a mandatory field and should not be nil
     validates :title, presence: true, length: { minimum: 5 ,  maximum: 50 }
     validates :description, presence:true, length: { minimum: 10, maximum: 500 }
